f89c54509f29aad257ecc8effcef0ba8f5c768cd
nibral <nibral@dancingstarson.me>
2016-01-25 13:55:08 +0900

    ES6

--- twitter.js
+++ twitter.js
@@ -1,9 +1,9 @@
 'use strict'
 
 // Twitterアクセス設定
-var Twitter = require('twitter');
-var config = require('./config');
-var client = new Twitter({
+const Twitter = require('twitter');
+const config = require('./config');
+const client = new Twitter({
     consumer_key:           config.TWITTER_CONSUMER_KEY,
     consumer_secret:        config.TWITTER_CONSUMER_SECRET,
     access_token_key:       config.TWITTER_ACCESS_TOKEN,
@@ -11,11 +11,11 @@ var client = new Twitter({
 });
 
 // 指定したscreen_nameのユーザのLike数を取得する
-var getFavoriteCount = function(screenName, callback) {
-    var params = {
+module.exports.getFavoriteCount = (screenName, callback) {
+    const params = {
         screen_name: screenName
     }    
-    client.get('users/show', params, function(error, info, response){
+    client.get('users/show', params, (error, info, response) => {
         if(error) {
             throw error;
         }
@@ -24,30 +24,30 @@ var getFavoriteCount = function(screenName, callback) {
 }
 
 // 指定したscreen_nameのユーザのLikeを取得し、画像URLの配列を返す
-module.exports.getFavoriteImageURLArray = function(screenName, numOfMaxTweets, callback) {
-    var numOfTweetsPerRequest = (numOfMaxTweets > 200) ? 200 : numOfMaxTweets;
+module.exports.getFavoriteImageURLArray = (screenName, callback) {
+    const numOfTweetsPerRequest = 200;
     
     // パラメータ設定
-    var params = {
+    const params = {
         screen_name: screenName,
         count: numOfTweetsPerRequest
     };
     
     // リクエスト発行
-    client.get('favorites/list', params, function(error, tweets, response) {
+    client.get('favorites/list', params, (error, tweets, response) => {
         if(error) {
             throw error;
         }
         
         // 画像が含まれる場合のみURLを取得して配列に追加
-        var imageURLs = new Array();
-        for(var tj = 0; tj < tweets.length; tj++) {
-            var tweet = tweets[tj];
-            // var tweetID = tweet.id_str;
+        let imageURLs = new Array();
+        for(let tj = 0; tj < tweets.length; tj++) {
+            const tweet = tweets[tj];
+            const tweetID = tweet.id_str;
             if(tweet.extended_entities) {
-                var medias = tweet.extended_entities.media;
+                const medias = tweet.extended_entities.media;
                 if(medias) {
-                    for(var mk = 0; mk < medias.length; mk++) {
+                    for(let mk = 0; mk < medias.length; mk++) {
                         if(medias[mk].type === 'photo') {
                             imageURLs.push(medias[mk].media_url_https + ':orig');
                         }
